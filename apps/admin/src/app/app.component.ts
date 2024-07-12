cdff5f76bc149bfb232777910c1bb84ae5c3b56a
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2024-07-12 22:09:02 +0530

    first commit

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@Component({
+  standalone: true,
+  imports: [DashboardComponent, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'admin';
+}
---
+++ apps/admin/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const appRoutes: Route[] = [{
+  path: '',
+  component: DashboardComponent
+}];
