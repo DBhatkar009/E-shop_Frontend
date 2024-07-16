e41ac7c3df8bc1ad043432b5cf7a2b97ef047268
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2024-07-16 16:10:26 +0530

    installing primeng flex for grid usage

--- apps/ngshop/src/app/app.component.ts
+++ apps/ngshop/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BannerComponent } from 'ui/src/lib/banner/banner.component';
 import { OrdersComponent } from 'orders/src/lib/orders/orders.component';
+import { RouterModule } from '@angular/router';
+// import { AppModule } from '../app.module';
 
 @Component({
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [OrdersComponent, BannerComponent, ProductListComponent, HomePageComponent, HeaderComponent, FooterComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html'
