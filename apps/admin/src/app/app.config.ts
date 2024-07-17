eb763a5f28d95399a49de60f29f71f43bf907280
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2024-07-17 21:48:03 +0530

    Include Primeng Packages and Created Admin.module.ts file for Admin Project

--- apps/admin/src/app/app.config.ts
+++ apps/admin/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimations(),
+    importProvidersFrom([FormsModule, ReactiveFormsModule])
   ],
 };
--- apps/admin/src/app/pages/dashboard/dashboard.component.ts
+++ apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TabViewModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
