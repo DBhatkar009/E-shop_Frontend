b12c8eb786574b4682564cebbe1cf87032041bd8
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2024-07-17 22:08:26 +0530

    Added New Two Components.

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   standalone: true,
-  imports: [DashboardComponent, RouterModule],
+  imports: [DashboardComponent, ShellComponent, SidebarComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Route } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
 
 export const appRoutes: Route[] = [{
   path: '',
-  component: DashboardComponent
+  component: ShellComponent
 }];
